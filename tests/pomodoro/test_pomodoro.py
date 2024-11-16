841d00d162046ba8c49dd9a488ea5cf30d7292b1
Joey <josephrobertlopez@gmail.com>
2024-11-15 18:06:26 -0600

    Update test_pomodoro.py

--- tests/pomodoro/test_pomodoro.py
+++ tests/pomodoro/test_pomodoro.py
@@ -1,19 +1,26 @@
 from src.pomodoro.pomodoro import main
+import pytest
 
 
-def test_main_positive_minutes(mock_pomodoro_deps, mock_sleep):
-    """Test with valid positive minutes."""
-    # Use `MockFixture` to mock `get_env_var` and `play_alarm_sound`
-    # Run the main function with positive minutes argument
-    main("-m", "26")
+def test_main(mock_pomodoro_deps, mock_sleep):
+    """Test main method of pomodoro."""
+    SOUND_FILE = "resources/sounds/alarm_sound.wav"
 
-    # Assert that `time.sleep` was called with the correct duration
-    mock_sleep.assert_called_once_with(26 * 60)
+    mock_get_env_var = mock_pomodoro_deps.get_mock_obj("get_env_var")
+    mock_play_alarm_sound = mock_pomodoro_deps.get_mock_obj("play_alarm_sound")
 
-    # Assert that `get_env_var` was called with "SOUND_FILE"
-    (mock_pomodoro_deps.get_mock_obj("get_env_var")
-     .assert_called_once_with("SOUND_FILE"))
+    # Test valid input
+    main("-m", "25")
+    mock_sleep.assert_called_once_with(25 * 60)
+    mock_get_env_var.assert_called_once_with("SOUND_FILE")
+    mock_play_alarm_sound.assert_called_once_with(SOUND_FILE)
 
-    # Assert that `play_alarm_sound` was called with "alarm.wav"
-    (mock_pomodoro_deps.get_mock_obj("play_alarm_sound")
-     .assert_called_once_with("resources/sounds/alarm_sound.wav"))
+    # Test invalid integer input
+    mock_pomodoro_deps.reset_mocks()
+    mock_sleep.reset_mock()
+    main("-m", "0")
+
+    # Test with non integer input
+    mock_pomodoro_deps.reset_mocks()
+    mock_sleep.reset_mock()
+    assert pytest.raises(SystemExit, main, "-m", "'a'").value.code == 2
