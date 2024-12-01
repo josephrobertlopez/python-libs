73634b133f8043506964abd1d1f5534eaf390dab
Joey <josephrobertlopez@gmail.com>
2024-12-01 10:49:55 -0600

    Update test_env_checks.py

--- tests/utils/env_checks/test_env_checks.py
+++ tests/utils/env_checks/test_env_checks.py
@@ -1,12 +1,9 @@
-import os
 import pytest
-from unittest import mock
 from src.utils.env_checks.env_checks import (
     get_running_in_pyinstaller,
     get_env_var,
     load_environment_variables,
 )
-from src.utils.test.MockManager import MockManager
 
 
 def test_get_running_in_pyinstaller(mock_sys):
@@ -26,7 +23,6 @@ def test_get_env_var(mock_os):
     with pytest.raises(KeyError, match="Environment variable "
                                        "'NON_EXISTENT_VAR' not found"):
         get_env_var("NON_EXISTENT_VAR")
-
 
 
 def test_load_environment_variables_file_exists(mock_os):
