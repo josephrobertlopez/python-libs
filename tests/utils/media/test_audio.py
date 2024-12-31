72ba505ea63501df560b9b356f3488de52783d90
Joey <josephrobertlopez@gmail.com>
2024-12-30 16:21:25 -0800

    Update test_audio.py

--- tests/utils/media/test_audio.py
+++ tests/utils/media/test_audio.py
@@ -2,10 +2,10 @@ def test_load_sound(mock_singleton_setup, mixer, pygame_mixer_audio):
     with pygame_mixer_audio:
         mixer.load_sound("Fake_sound.wav")
         pygame_mixer_audio.get_mock("Sound").assert_called_once()
-    with pygame_mixer_audio.remove_patch("Sound") as m:
-        del m
+    with pygame_mixer_audio.remove_patch("Sound"):
         mixer.load_sound("Fake_sound.wav")
-
+    with pygame_mixer_audio.remove_patch("init"):
+        mixer.load_sound("Fake_sound.wav")
 
 def test_play_sound(mock_singleton_setup, mixer, pygame_mixer_audio):
     # Mocking the pygame.mixer.Sound class to simulate successful sound playback
@@ -13,9 +13,10 @@ def test_play_sound(mock_singleton_setup, mixer, pygame_mixer_audio):
         # Run the play_alarm_sound method
         # Assert that the play method was called once
         mixer.load_sound("Fake_sound.wav")
-        mixer.play_sound(until_time=0)
+        mixer.play_sound(until_time=.001)
         pygame_mixer_audio.get_mock("Sound")().play.assert_called_once()
-
+        # Add for coverage
+        mixer.play_sound()
 
 def test_is_sound_playing(mock_singleton_setup, mixer, pygame_mixer_audio):
     """Test that is_sound_playing returns True when sound is playing."""
