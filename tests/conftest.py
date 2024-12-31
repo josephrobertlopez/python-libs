6bdf4689f0b15026ed801903604a819cd6d344d2
Joey <josephrobertlopez@gmail.com>
2024-12-30 16:13:15 -0800

    conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ from src.utils.abstract.abstract_singleton import (
     AbstractSingleton,
     SampleConcreteSingleton,
 )
+from src.utils.media.audio import PygameMixerSoundSingleton
 from src.utils.test.MockContextManager import MockContextManager
 
 
@@ -70,6 +71,43 @@ def mock_pomodoro_deps():
     return MockContextManager(target_path=mock_path, method_behaviors=default_behaviors)
 
 
+@pytest.fixture
+def sample_concrete_singleton():
+    """Fixture to instantiate the SampleConcreteSingleton."""
+    return SampleConcreteSingleton()
+
+
+@pytest.fixture
+def pygame_mixer_audio():
+    """Fixture to provide a PygameMixerAudio instance with mocked dependencies."""
+    # set up the mock behaviors for methods you want to mock
+    method_behaviors = {
+        "init": Mock(),
+        "music.get_busy": False,
+        "music.play": Mock(),
+        "music.pause": Mock(),
+        "music.set_volume": Mock(),
+    }
+
+    class_values = {"Sound": {"get_num_channels": 1}}
+    manager = MockContextManager(
+        target_path="pygame.mixer",
+        method_behaviors=method_behaviors,
+        class_values=class_values,
+    )
+    return manager
+
+
+@pytest.fixture
+def mixer(pygame_mixer_audio):
+    """Fixture to initialize PygameMixerAudio instance."""
+    # Initialize PygameMixerAudio instance before each test
+    mixer = PygameMixerSoundSingleton()
+    # Ensure the singleton is set up in the context
+    with pygame_mixer_audio:
+        yield mixer
+
+
 @pytest.fixture()
 def mock_singleton_setup():
     """Fixture to mock AbstractSingleton setup and ensure it's called only once."""
@@ -80,9 +118,3 @@ def mock_singleton_setup():
         yield mock_setup
         # After each test, delete the setup to ensure it does not interfere with other tests
         del mock_setup
-
-
-@pytest.fixture
-def sample_concrete_singleton():
-    """Fixture to instantiate the SampleConcreteSingleton."""
-    return SampleConcreteSingleton()
--- tests/utils/media/test_audio.py
+++ tests/utils/media/test_audio.py
@@ -1,54 +1,4 @@
-from unittest.mock import patch, Mock
-from src.utils.abstract.abstract_singleton import AbstractSingleton
-from src.utils.test.MockContextManager import MockContextManager
-from src.utils.media.audio import PygameMixerSoundSingleton
-import pytest
-
-
-@pytest.fixture
-def pygame_mixer_audio():
-    """Fixture to provide a PygameMixerAudio instance with mocked dependencies."""
-    # set up the mock behaviors for methods you want to mock
-    method_behaviors = {
-        "init": Mock(),
-        "music.get_busy": False,
-        "music.play": Mock(),
-        "music.pause": Mock(),
-        "music.set_volume": Mock(),
-    }
-
-    class_values = {"Sound": {"get_num_channels": 1}}
-    manager = MockContextManager(
-        target_path="pygame.mixer",
-        method_behaviors=method_behaviors,
-        class_values=class_values,
-    )
-    return manager
-
-
-@pytest.fixture
-def mixer(pygame_mixer_audio):
-    """Fixture to initialize PygameMixerAudio instance."""
-    # Initialize PygameMixerAudio instance before each test
-    mixer = PygameMixerSoundSingleton()
-    # Ensure the singleton is set up in the context
-    with pygame_mixer_audio:
-        yield mixer
-
-
-@pytest.fixture(autouse=True)
-def mock_singleton_setup():
-    """Fixture to mock AbstractSingleton setup and ensure it's called only once."""
-    # Patch the setup method in AbstractSingleton to prevent the singleton check
-    with patch.object(AbstractSingleton, "setup", Mock()) as mock_setup:
-        # Ensure the singleton is set up before each test
-        mock_setup()
-        yield mock_setup
-        # After each test, delete the setup to ensure it does not interfere with other tests
-        del mock_setup
-
-
-def test_load_sound(mixer, pygame_mixer_audio):
+def test_load_sound(mock_singleton_setup, mixer, pygame_mixer_audio):
     with pygame_mixer_audio:
         mixer.load_sound("Fake_sound.wav")
         pygame_mixer_audio.get_mock("Sound").assert_called_once()
@@ -57,17 +7,17 @@ def test_load_sound(mixer, pygame_mixer_audio):
         mixer.load_sound("Fake_sound.wav")
 
 
-def test_play_alarm_sound(mixer, pygame_mixer_audio):
+def test_play_sound(mock_singleton_setup, mixer, pygame_mixer_audio):
     # Mocking the pygame.mixer.Sound class to simulate successful sound playback
     with pygame_mixer_audio:
         # Run the play_alarm_sound method
         # Assert that the play method was called once
         mixer.load_sound("Fake_sound.wav")
-        mixer.play_sound()
+        mixer.play_sound(until_time=0)
         pygame_mixer_audio.get_mock("Sound")().play.assert_called_once()
 
 
-def test_is_sound_playing(mixer, pygame_mixer_audio):
+def test_is_sound_playing(mock_singleton_setup, mixer, pygame_mixer_audio):
     """Test that is_sound_playing returns True when sound is playing."""
     with pygame_mixer_audio:
         assert not mixer.is_sound_playing()
