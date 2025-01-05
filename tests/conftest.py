d761ba650ae6f732023b5f923a312a668c1bbd4e
Joey <josephrobertlopez@gmail.com>
2025-01-05 16:00:36 -0600

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -66,17 +66,6 @@ def mock_context():
 
 
 @pytest.fixture
-def mock_pomodoro_deps():
-    mock_path = "src.pomodoro.pomodoro"
-    SOUND_FILE = "resources/sounds/alarm_sound.wav"
-    default_behaviors = {
-        "get_env_var": SOUND_FILE,
-        "play_alarm_sound": None,
-    }
-    return MockContextManager(target_path=mock_path, method_behaviors=default_behaviors)
-
-
-@pytest.fixture
 def sample_concrete_singleton():
     """Fixture to instantiate the SampleConcreteSingleton."""
     return SampleConcreteSingleton()
